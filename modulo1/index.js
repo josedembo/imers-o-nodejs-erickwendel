8296921db9f2b6f4479471f62aef336b063b2f38
josedembo <josedembo18@aluno.unilab.edu.br>
2021-12-22 06:40:48 -0300

    refatorando o codigo usando promises

--- modulo1/index.js
+++ modulo1/index.js
@@ -4,28 +4,35 @@
 2 obter o endereço do usuário pelo id
 */
 
+const util = require("util");
+const obterEnderecoAsync = util.promisify(obterEndereco);
 
-function obterUsuario(callback) {
-    setTimeout(function () {
-        return callback(null, {
-            id: 1,
-            nome: "Aladin",
-            dataNascimento: new Date()
-        })
 
-    }, 1000);
 
+function obterUsuario() {
+    return new Promise(function resolvePromise(resolve, reject) {
+        setTimeout(function () {
+            return resolve({
+                id: 1,
+                nome: "Aladin",
+                dataNascimento: new Date()
+            });
+
+        }, 1000);
+
+    });
 }
 
 
-function obterTelefone(usuarioId, callback) {
-    setTimeout(function () {
-        return callback(null, {
-            telefone: "2844950",
-            ddd: "85"
-        });
-
-    }, 2000);
+function obterTelefone(usuarioId) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            return resolve({
+                telefone: "2844950",
+                ddd: "85"
+            });
+        }, 2000);
+    });
 
 }
 
@@ -43,37 +50,72 @@ function obterEndereco(usuarioId, callback) {
 }
 
 
-obterUsuario(function resolveUsuario(error, usuario) {
+const usuarioPromise = obterUsuario();
+// console.log(usuarioPromise)
 
-    if (error) {
-        console.error("Deu erro na busca do usuario");
-        return;
-    }
-
-    obterTelefone(usuario.id, function resolveTelefone(error1, telefone) {
-        if (error1) {
-            console.error("deu na busca do telefone:", error1);
-            return;
-        }
-
-        obterEndereco(usuario.id, function resolveObterEndereco(error2, endereco) {
-
-            if (error2) {
-                console.log("Deu erro na busca do endereco: ", error2)
-                return;
-            }
-
-            console.log(`nome: ${usuario.nome}\ntelefone: (${telefone.ddd})${telefone.telefone}\nendereco: ${endereco.rua}`)
-
+usuarioPromise
+    .then(function (usuario) {
+        return obterTelefone(usuario.id)
+            .then(function (telefone) {
+                return {
+                    usuario: {
+                        nome: usuario.nome,
+                        id: usuario.id
+                    },
+                    telefone: telefone
+                }
+            })
+    })
+    .then(function (resultado) {
+        const obterEnderco = obterEnderecoAsync(resultado.usuario.id);
+        return obterEnderco.then(function resolveEndereco(endereco) {
             return {
-                nome: usuario.nome,
-                telefone: `(${telefone.ddd})${telefone.telefone}`,
-                endereco: `${endereco.rua}`
+                usuario: {
+                    ...resultado.usuario,
+                    telefone: resultado.telefone,
+                    endereco: endereco
+                }
             }
         })
+    })
+    .then(function (resolve) {
+        console.log("resultado: ", resolve);
+    })
+    .catch(function (error) {
+        console.error("Deu erro:", error);
     });
 
-});
+// obterUsuario(function resolveUsuario(error, usuario) {
+
+//     if (error) {
+//         console.error("Deu erro na busca do usuario");
+//         return;
+//     }
+
+//     obterTelefone(usuario.id, function resolveTelefone(error1, telefone) {
+//         if (error1) {
+//             console.error("deu na busca do telefone:", error1);
+//             return;
+//         }
+
+//         obterEndereco(usuario.id, function resolveObterEndereco(error2, endereco) {
+
+//             if (error2) {
+//                 console.log("Deu erro na busca do endereco: ", error2)
+//                 return;
+//             }
+
+//             console.log(`nome: ${usuario.nome}\ntelefone: (${telefone.ddd})${telefone.telefone}\nendereco: ${endereco.rua}`)
+
+//             return {
+//                 nome: usuario.nome,
+//                 telefone: `(${telefone.ddd})${telefone.telefone}`,
+//                 endereco: `${endereco.rua}`
+//             }
+//         })
+//     });
+
+// });
 
 
 
